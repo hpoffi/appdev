4a41c8c3f5f4affe9f3cfbd147bf38ef964e9547
Harshit Patel <harshitpatel@Harshits-MacBook-Air.local>
2024-02-17 18:17:00 +0530

    Added scrollview

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View,ScrollView,Image,StyleSheet,TextInput } from "react-native";
+import icon from "./assets/icon.png";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text numberOfLines={2} ellipsizeMode="tail" style={{ fontSize: 40, fontWeight: "bold" }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc
+      </Text>
+      <Image source={icon}
+       style={{width: 100, height: 100, backgroundColor: "red"}} 
+       resizeMode="contain"/>
+       <TextInput placeholder ="what's your name"/>
     </View>
+    </ScrollView>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App; 
